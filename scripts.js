26bed871d6784f71cd6b06bad53d568f8c0e044c
FreshOceans <mperezz398@gmail.com>
2017-06-05 13:43:38 -0400

    added hexClock

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
 var TimeWizard = {
-  checkTime: function(i) {
-    // console.log("== checkTime ==");
-    return (i < 10) ? "0" + i : i;
+  initialize: function() {
+    TimeWizard.updateTime()
   },
-  startTime: function() {
+  updateTime: function() {
     // console.log("== startTime ==");
     var today = new Date();
     var h = today.getHours();
@@ -11,20 +10,44 @@ var TimeWizard = {
     var s = today.getSeconds();
     m = TimeWizard.checkTime(m);
     s = TimeWizard.checkTime(s);
-    document.getElementById("realTime").innerHTML = h + ":" + m + ":" + s;
+    var hH = TimeWizard.hexTime(h);
+    var mH = TimeWizard.hexTime(m);
+    var sH = TimeWizard.hexTime(s);
+    mH = TimeWizard.checkTime(mH);
+    sH = TimeWizard.checkTime(sH);
+    hH = TimeWizard.checkTime(hH);
+    document.getElementById("realClock").innerHTML = h + ":" + m + ":" + s;
+    document.getElementById("hexClock").innerHTML = hH + ":" + mH + ":" + sH;
     t = setTimeout(function() {
-      TimeWizard.startTime()
-    }, 500);
+      TimeWizard.updateTime()
+    }, 1000);
   },
-  hexTime: function() {
-    console.log("== hexTime ==");
+  hexTime: function(num) {
+    // console.log("== hexTime ==");
+    hexString = num.toString(16);
+    // console.log("hexString:", hexString);
+    return hexString;
+  },
+  checkTime: function(i) {
+    // console.log("== checkTime ==");
+    return (i < 10) ? "0" + i : i;
   }
+  // changeColors: function() {
+  //   var currentTime = new Date().getSeconds();
+  //   colors: ["#3E69F6", "#0FDB38", "#CB3025"];
+  //   if (7 <= currentTime && currentTime < 20) {
+  //       if (document.body) {
+  //           document.body.background = "http://itsnotch.com/tumblr/images/daytime_bg.jpg";
+  //       }
+  //   }
+  //   else {
+  //       if (document.body) {
+  //           document.body.background = "http://itsnotch.com/tumblr/images/nighttime_bg.jpg";
+  //       }
+  //   }
+  // }
+
 };
+TimeWizard.initialize();
 
-
-
-
-
-TimeWizard.checkTime();
-TimeWizard.startTime();
-TimeWizard.hexTime();
+// TimeWizard.changeColors();
